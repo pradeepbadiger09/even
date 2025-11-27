1bf43105ca6cf6d925a9a92e251e08627546a9da
pradeepbadiger09 <pradeepbadiger618@gmail.com>
2025-11-27 14:52:55 +0530

    Create even.py

---
+++ even.py
@@ -0,0 +1,16 @@
+import sys
+if len(sys.argv) < 2:
+    print("Usage: python even_odd_count.py <num1> <num2> ...")
+    sys.exit()
+numbers = [int(x) for x in sys.argv[1:]]
+
+even_count = 0
+odd_count = 0
+for n in numbers:
+    if n % 2 == 0:
+        even_count += 1
+    else:
+        odd_count += 1
+
+print("Even Count:", even_count)
+print("Odd Count:", odd_count)
